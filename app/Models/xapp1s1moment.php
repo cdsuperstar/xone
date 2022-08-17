f255e022d4ead8ff5e35b50eca65b564ed1c9e71
Luke Lin <181977814@qq.com>
2022-08-17 16:30:50 +0800

    Fix bug of foreign key.

--- app/Models/xapp1s1moment.php
+++ app/Models/xapp1s1moment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Traits\InteractsWithComment;
 use App\Traits\InteractsWithThumb;
+use App\Traits\InteractsWithUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
@@ -11,7 +12,7 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 
 class xapp1s1moment extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia, InteractsWithComment, Interactswiththumb;
+    use HasFactory, InteractsWithMedia, InteractsWithComment, Interactswiththumb, InteractsWithUser;
     protected $fillable = ['note','type'];
 
     public function registerMediaCollections(): void
@@ -24,9 +25,5 @@ class xapp1s1moment extends Model implements HasMedia
     {
         return $date->format('Y-m-d H:i:s');
 //        return $date->toW3cString();
-    }
-
-    public function user_pub(){
-        return $this->belongsTo('App\Models\User','user_id')->select(['id','created_at']);
     }
 }
