9fb9d9e17051029f1eafe2296879263a298e74d1
Luke Lin <181977814@qq.com>
2022-08-18 15:37:24 +0800

    Add search mark.

--- database/migrations/2022_08_04_145022_create_xapp1s1profiles_table.php
+++ database/migrations/2022_08_04_145022_create_xapp1s1profiles_table.php
@@ -25,33 +25,33 @@ return new class extends Migration {
             $table->string('approval')->default('待审核')->nullable();// 审核状态
 
             // 自动生成
-            $table->date('birthday')->nullable();
-            $table->string('constellation', 6)->nullable();
+            $table->date('birthday')->nullable(); // S
+            $table->string('constellation', 6)->nullable(); // S
 
             $table->string('nickname', 30)->nullable();
             //0 - 未知的性别
             //1 - 男性
             //2 - 女性
             //9 - 未说明的性别
-            $table->string('sex', 1)->nullable();//性别
-            $table->integer('height')->nullable();//身高
-            $table->integer('incomebegin')->nullable();//收入开始
-            $table->integer('incomeend')->nullable();//收入结束
+            $table->string('sex', 1)->nullable();//性别 S
+            $table->integer('height')->nullable();//身高 S
+            $table->integer('incomebegin')->nullable();//收入开始 S
+            $table->integer('incomeend')->nullable();//收入结束 S
             $table->string('workaddress')->nullable();//工作地点
-            $table->string('eduback')->nullable();//学历
-            $table->string('marriage')->nullable();//婚姻状况
+            $table->string('eduback')->nullable();//学历 S
+            $table->string('marriage')->nullable();//婚姻状况 S
 
             $table->string('nationality')->nullable();//名族
-            $table->string('career')->nullable();//职业
+            $table->string('career')->nullable();//职业 S
             $table->string('nativeplace')->nullable();//籍贯
-            $table->integer('weight')->nullable();//体重
-            $table->string('housesitu')->nullable();//购房情况
-            $table->string('carsitu')->nullable();//购车情况
+            $table->integer('weight')->nullable();//体重 S
+            $table->string('housesitu')->nullable();//购房情况 S
+            $table->string('carsitu')->nullable();//购车情况 S
 
-            $table->string('smokesitu')->nullable();//吸烟情况
-            $table->string('drinksitu')->nullable();//喝酒情况
+            $table->string('smokesitu')->nullable();//吸烟情况 S
+            $table->string('drinksitu')->nullable();//喝酒情况 S
 
-            $table->string('childrensitu')->nullable();//小孩情况
+            $table->string('childrensitu')->nullable();//小孩情况 S
 
             $table->text('memo')->nullable();//文字简介
 
