b9dbfd80141e4950f6cb4871d136908ca24071d9
Luke Lin <181977814@qq.com>
2023-08-21 12:11:57 +0800

    Update app1s1 seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
      * @return void
      * 系统
      * docker-compose exec postgres pg_dump -O -x -a --column-inserts xone -U default -t oauth_clients -t users -t z_modules -t z_userprofiles -t roles -t role_z_module -t z_units -t model_has_roles -t permissions -t model_has_permissions -t user_z_unit -t media >../xone/database/seeders/dump.sql
-     * xapp1s1业务
-     * ocker-compose exec postgres pg_dump -O -x -a --column-inserts xone -U default -t xapp1s1categs -t xapp1s1profiles -t xapp1s1profiles -t xapp1s1shops  >>../xone/database/seeders/dump.sql
+     * xapp1s1 APP
+     * docker-compose exec postgres pg_dump -O -x -a --column-inserts xone -U default -t xapp1s1profiles -t xapp1s1categs -t xapp1s1shops -t xapp1s1products -t xapp1s1moments -t xapp1s1activates -t xapp1s1slots -t comments -t thumbs -t scores -t likes >>../xone/database/seeders/dump.sql
      *
      */
     public function run()
